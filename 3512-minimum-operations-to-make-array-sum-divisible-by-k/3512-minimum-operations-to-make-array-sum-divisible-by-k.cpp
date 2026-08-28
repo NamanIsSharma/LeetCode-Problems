d950b89cbe923d9af7ae42eb018ac69ab50278cd
Naman Sharma <namansharmajnv@gmail.com>
2026-08-28 22:35:34 +0530

    Time: 0 ms (100%), Space: 45.3 MB (68.3%) - LeetHub

---
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -0,0 +1,10 @@
+class Solution { /// if we noticed we just have to return the remainder after dividing the sum from k (easy - peasy)
+public:
+    int minOperations(vector<int>& nums, int k) {
+        long long sum = 0; //intially sum is zero
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i]; // update sum till adding last element
+        }
+        return sum % k; // remainder = minm operation 
+    }
+};
