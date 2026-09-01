a794aeae9d12614a6afa985f33b70c68ba97e44b
Naman Sharma <namansharmajnv@gmail.com>
2026-09-01 23:30:42 +0530

    Time: 0 ms (100%), Space: 15.3 MB (37.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,6 +1,7 @@
-class Solution {
-public:
+class Solution {               // although it will done using binary search with tc o(log n)
+public:                       // i'll come back to solve this at optimal once i get binary search
     int search(vector<int>& nums, int target) {
+        // i just search linearly with O(N) tc
         for(int i=0; i<nums.size(); i++){
             if(nums[i] == target){
                 return i;
